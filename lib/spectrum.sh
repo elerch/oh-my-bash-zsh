2a7620ffd52a728b9edfdfb5a8b2c4af301d99c8
Emil Lerch <emil@lerch.org>
2017-05-03 11:55:12 -0700

    rename/replace spectrum

---
+++ lib/spectrum.sh
@@ -0,0 +1,4 @@
+function all-colors() {
+  for code in $(seq -w 0 255); do for attr in 0 1; do printf "%s-%03s %bTest%b\n" "${attr}" "${code}" "\e[${attr};38;05;${code}m" "\e[m"; done; done | column -c $((COLUMNS*2))
+}
+
