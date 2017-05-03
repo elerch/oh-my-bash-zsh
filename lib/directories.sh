c70c190a8acde50082ce250c72f8ab069bc6a869
Emil Lerch <emil@lerch.org>
2017-05-03 11:36:15 -0700

    directories bash support/streamlining

---
+++ lib/directories.sh
@@ -0,0 +1,9 @@
+# List directory contents
+alias lsa='ls -lah'
+alias l='ls -lah'
+alias ll='ls -lh'
+alias la='ls -lAh'
+
+# Push and pop directories on directory stack
+alias pu='pushd'
+alias po='popd'
