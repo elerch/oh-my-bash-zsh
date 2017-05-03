f0e096309aa95537411e63f638ff282529a1d479
Emil Lerch <emil@lerch.org>
2017-05-03 09:24:21 -0700

    remove more plugin stuff

--- oh-my-zsh.sh
+++ oh-my-zsh.sh
@@ -28,23 +28,6 @@ for config_file ($ZSH/lib/*.zsh); do
   source $config_file
 done
 
-
-is_plugin() {
-  local base_dir=$1
-  local name=$2
-  test -f $base_dir/plugins/$name/$name.plugin.zsh \
-    || test -f $base_dir/plugins/$name/_$name
-}
-# Add all defined plugins to fpath. This must be done
-# before running compinit.
-for plugin ($plugins); do
-  if is_plugin $ZSH_CUSTOM $plugin; then
-    fpath=($ZSH_CUSTOM/plugins/$plugin $fpath)
-  elif is_plugin $ZSH $plugin; then
-    fpath=($ZSH/plugins/$plugin $fpath)
-  fi
-done
-
 # Figure out the SHORT hostname
 if [[ "$OSTYPE" = darwin* ]]; then
   # macOS's $HOST changes with dhcp, etc. Use ComputerName if possible.
