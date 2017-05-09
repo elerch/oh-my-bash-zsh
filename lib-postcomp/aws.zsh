d029fbf128ebda00d1ea404b2dd7a18687a1c206
Emil Lerch <emil@lerch.org>
2017-05-08 18:21:00 -0700

    support for missing aws_zsh_completer.sh

--- lib-postcomp/aws.zsh
+++ lib-postcomp/aws.zsh
@@ -30,8 +30,10 @@ compctl -K aws_profiles asp
 if _homebrew-installed && _awscli-homebrew-installed ; then
   _aws_zsh_completer_path=$(brew --prefix awscli)/libexec/bin/aws_zsh_completer.sh
 else
-  _aws_zsh_completer_path=$(which aws_zsh_completer.sh)
+  if hash aws_zsh_completer.sh 2> /dev/null; then
+    _aws_zsh_completer_path=$(which aws_zsh_completer.sh)
+  fi
 fi
 
-[ -x $_aws_zsh_completer_path ] && source $_aws_zsh_completer_path
-unset _aws_zsh_completer_path
+[ -x "$_aws_zsh_completer_path" ] && source $_aws_zsh_completer_path
+[ ! -z "$_aws_zsh_completer_path" ] && unset _aws_zsh_completer_path
